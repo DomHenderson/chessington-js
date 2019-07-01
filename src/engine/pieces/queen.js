0d9700ca7a22bf9081bdb1f02c61fa1e6b2f7357
Dom Henderson <dominic.henderson@softwire.com>
2019-07-01 11:22:25 +0100

    Added queen movement

--- src/engine/pieces/queen.js
+++ src/engine/pieces/queen.js
@@ -1,4 +1,5 @@
 import Piece from './piece';
+import Square from '../square';
 
 export default class Queen extends Piece {
     constructor(player) {
@@ -6,6 +7,34 @@ export default class Queen extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let square = board.findPiece(this);
+        let moves = [];
+        //Rooks can move to any square with one matching coordinate
+        for (let i = 0; i < 8; ++i) {
+            if (i !== square.row) {
+                moves.push(new Square(i, square.col));
+            }
+
+            if (i !== square.col) {
+                moves.push(new Square(square.row, i));
+            }
+
+            let offset = i - square.row;
+
+            //Cannot move to own square
+            if (offset === 0) {
+                continue;
+            }
+
+            //Can move to any square where horizontal and vertical offset have the same magnitude
+            if (square.col + offset < 8) {
+                moves.push(square.createWithOffset(offset, offset));
+            }
+
+            if (square.col - offset >= 0) {
+                moves.push(square.createWithOffset(offset, -offset));
+            }
+        }
+        return moves;
     }
 }
