2343bd8274e6a98e572baffe55aeac59cdf768c2
Dom Henderson <dominic.henderson@softwire.com>
2019-07-01 14:01:40 +0100

    Pieces can no longer leave the board

--- src/engine/movement/Movement.js
+++ src/engine/movement/Movement.js
@@ -3,7 +3,7 @@ export default function ( directions, limit ) {
     return (square,board) => {
         let moves = [];
         for (let direction of directions) {
-            for (let i = 1; i < limit; ++i) {
+            for (let i = 1; i <= limit; ++i) {
                 let newSquare = square.createWithOffset(i * direction.x, i * direction.y);
                 if ( newSquare.row < 0 || newSquare.row > 7 || newSquare.col < 0 || newSquare.col > 7 || board.getPiece(newSquare) ) {
                     break;
--- src/engine/movement/PawnMovement.js
+++ src/engine/movement/PawnMovement.js
@@ -14,14 +14,14 @@ function getPawnMoves ( direction, homeRow ) {
     return (square,board) => {
         let moves = [];
         //Check that the space in front is free
-        if ( board.getPiece(square.createWithOffset(direction,0) ) ) {
+        if ( square.row + direction < 0 || square.row + direction >= 8 || board.getPiece(square.createWithOffset(direction,0) ) ) {
             return moves;
         }
 
         moves.push(square.createWithOffset(direction,0));
 
         //Add 2 square move if the pawn is on the home row and there isn't anything in the way
-        if ( square.row === homeRow && !board.getPiece(square.createWithOffset(2*direction,0))) {
+        if ( square.row + 2*direction < 0 || square.row + 2*direction >= 8 || square.row === homeRow && !board.getPiece(square.createWithOffset(2*direction,0))) {
             moves.push(square.createWithOffset(2*direction,0));
         }
 
