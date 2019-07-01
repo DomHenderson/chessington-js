0f07652bd4728decd96f2a67ebe89a37fed7420a
Dom Henderson <dominic.henderson@softwire.com>
2019-07-01 11:33:34 +0100

    Added knight movement

--- src/engine/pieces/knight.js
+++ src/engine/pieces/knight.js
@@ -6,6 +6,20 @@ export default class Knight extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let square = board.findPiece(this);
+
+        let moves = [];
+        moves.push(square.createWithOffset(1,2));
+        moves.push(square.createWithOffset(2,1));
+        moves.push(square.createWithOffset(1,-2));
+        moves.push(square.createWithOffset(2,-1));
+        moves.push(square.createWithOffset(-1,2));
+        moves.push(square.createWithOffset(-2,1));
+        moves.push(square.createWithOffset(-1,-2));
+        moves.push(square.createWithOffset(-2,-1));
+
+        moves = moves.filter ( square => square.row >= 0 && square.row < 8 && square.col >= 0 && square.col < 8 );
+
+        return moves;
     }
 }
