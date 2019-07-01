c5951e3b7d753338875defa318974681df78459f
Dom Henderson <dominic.henderson@softwire.com>
2019-07-01 13:56:28 +0100

    Bishops cannot move through pieces

--- src/engine/movement/DiagonalMovement.js
+++ src/engine/movement/DiagonalMovement.js
@@ -1,27 +1,6 @@
+import Movement from './Movement';
+
 export default function ( limit ) {
     limit = limit || 8;
-
-    return square => {
-        let moves = [];
-
-        for ( let i = 0; i < 8; ++i ) {
-            let offset = i - square.row;
-
-            // Cannot move to own square
-            if ( offset === 0 || Math.abs(offset) > limit ) {
-                continue;
-            }
-
-            // Can move to any square where horizontal and vertical offset have the same magnitude
-            if ( square.col + offset < 8 ) {
-                moves.push ( square.createWithOffset(offset,offset));
-            }
-
-            if ( square.col - offset >= 0 ) {
-                moves.push ( square.createWithOffset(offset,-offset));
-            }
-        }
-
-        return moves;
-    }
+    return Movement([{x:1,y:1},{x:-1,y:1},{x:1,y:-1},{x:-1,y:-1}],limit);
 }
