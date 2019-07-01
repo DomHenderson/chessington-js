abed8bb93160c6f258a5a218c6ca68bb3edbecda
Dom Henderson <dominic.henderson@softwire.com>
2019-07-01 12:25:42 +0100

    Added king movement

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,11 +1,9 @@
 import Piece from './piece';
+import DiagonalMovement from '../movement/DiagonalMovement';
+import LateralMovement from '../movement/LateralMovement';
 
 export default class King extends Piece {
     constructor(player) {
-        super(player);
-    }
-
-    getAvailableMoves(board) {
-        return new Array(0);
+        super(player,[LateralMovement(1),DiagonalMovement(1)]);
     }
 }
