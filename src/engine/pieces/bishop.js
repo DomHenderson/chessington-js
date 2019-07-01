c4433775206387632697ccdb47088e357b930985
Dom Henderson <dominic.henderson@softwire.com>
2019-07-01 11:14:00 +0100

    Added bishop moves

--- src/engine/pieces/bishop.js
+++ src/engine/pieces/bishop.js
@@ -6,6 +6,28 @@ export default class Bishop extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let square = board.findPiece(this);
+
+        let moves = [];
+
+        for ( let i = 0; i < 8; ++i ) {
+            let offset = i - square.row;
+
+            //Cannot move to own square
+            if ( offset === 0 ) {
+                continue;
+            }
+
+            //Can move to any square where horizontal and vertical offset have the same magnitude
+            if ( square.col + offset < 8 ) {
+                moves.push ( square.createWithOffset(offset,offset));
+            }
+
+            if ( square.col - offset >= 0 ) {
+                moves.push ( square.createWithOffset(offset,-offset));
+            }
+        }
+
+        return moves;
     }
 }
