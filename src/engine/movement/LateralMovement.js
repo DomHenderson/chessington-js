bb037e5a884b5cbe7b9db5ebb903cf341c564cc0
Dom Henderson <dominic.henderson@softwire.com>
2019-07-01 13:54:45 +0100

    Lateral movement can no longer go through pieces

--- src/engine/movement/LateralMovement.js
+++ src/engine/movement/LateralMovement.js
@@ -1,21 +1,6 @@
-import Square from '../square';
+import Movement from './Movement';
 
 export default function ( limit ) {
     limit = limit || 8;
-    return square => {
-        let moves = [];
-
-        //Can move to any square with one matching coordinate
-        for (let i = 0; i < 8; ++i) {
-            if (i !== square.row && Math.abs(i-square.row) < limit) {
-                moves.push(new Square(i, square.col));
-            }
-
-            if (i !== square.col && Math.abs(i-square.col) < limit) {
-                moves.push(new Square(square.row, i));
-            }
-        }
-
-        return moves;
-    }
+    return Movement ( [{x:1,y:0},{x:-1,y:0},{x:0,y:1},{x:0,y:-1}], limit );
 }
---
+++ src/engine/movement/Movement.js
@@ -0,0 +1,16 @@
+export default function ( directions, limit ) {
+    limit = limit || 8;
+    return (square,board) => {
+        let moves = [];
+        for (let direction of directions) {
+            for (let i = 1; i < limit; ++i) {
+                let newSquare = square.createWithOffset(i * direction.x, i * direction.y);
+                if ( newSquare.row < 0 || newSquare.row > 7 || newSquare.col < 0 || newSquare.col > 7 || board.getPiece(newSquare) ) {
+                    break;
+                }
+                moves.push(newSquare);
+            }
+        }
+        return moves;
+    };
+};
